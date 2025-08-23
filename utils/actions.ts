1b760edbc882aeac438ad118442514d22cc57a6c
gerwyn <gerwynsim@hotmail.com>
2025-08-24 01:50:28 +0800

    single job page/edit job

--- utils/actions.ts
+++ utils/actions.ts
@@ -108,7 +108,51 @@ export async function deleteJobAction(id: string): Promise<JobType | null> {
         clerkId: userId,
       },
     });
-    console.log(job)
+    console.log(job);
+    return job;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function getSingleJobAction(id: string): Promise<JobType | null> {
+  let job: JobType | null = null;
+  const userId = authenticateAndRedirect();
+
+  try {
+    job = await prisma.job.findUnique({
+      where: {
+        id,
+        clerkId: userId,
+      },
+    });
+  } catch (error) {
+    job = null;
+  }
+
+  if (!job) {
+    redirect("/jobs");
+  }
+
+  return job;
+}
+
+export async function updateJobAction(
+  id: string,
+  values: CreateAndEditJobType
+): Promise<JobType | null> {
+  const userId = authenticateAndRedirect();
+
+  try {
+    const job: JobType = await prisma.job.update({
+      where: {
+        id,
+        clerkId: userId,
+      },
+      data: {
+        ...values,
+      },
+    });
     return job;
   } catch (error) {
     return null;
