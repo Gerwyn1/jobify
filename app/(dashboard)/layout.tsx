38755d178522891d94c6663998e19b345343c47c
gerwyn <gerwynsim@hotmail.com>
2025-08-23 13:45:51 +0800

    setup/page

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+// import type { Metadata } from 'next';
+// import { Inter } from 'next/font/google';
+// import './globals.css';
+// const inter = Inter({ subsets: ['latin'] });
+
+// export const metadata: Metadata = {
+//   title: 'Jobify Dev',
+//   description: 'Job application tracking system for job hunters',
+// };
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
