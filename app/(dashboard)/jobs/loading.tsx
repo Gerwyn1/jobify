b31332803f11b3dcedda5abdd876b46dbcdeb99b
gerwyn <gerwynsim@hotmail.com>
2025-08-24 15:46:45 +0800

    stats/charts component

--- app/(dashboard)/jobs/loading.tsx
+++ app/(dashboard)/jobs/loading.tsx
@@ -1,4 +1,12 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 function loading() {
-  return <div className="text-xl font-medium capitalize">loading...</div>;
+  return (
+    <div className="p-8 grid sm:grid-cols-2 md:grid-cols-3  gap-4 rounded-lg border">
+      <Skeleton className="h-10" />
+      <Skeleton className="h-10 " />
+      <Skeleton className="h-10 " />
+    </div>
+  );
 }
 export default loading;
